b3501d0993bf7534dafc8e7740bc9f38dbe5be42
Raghdsmadi <raghadmohsmadi@hotmail.com>
2022-06-01 16:33:58 +0300

    lab02

---
+++ math_series/series.py
@@ -0,0 +1,25 @@
+def fibonacci(n):
+
+    if n==0 or n==1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def lucas(n):
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
+
+
+def sum_series(n, first=0, second=1):
+
+    if n==0:
+        return first
+    elif n==1:
+        return second
+    else:
+        return sum_series(n-1, first=first, second=second) + sum_series(n-2, first=first, second=second)
---
+++ math_series/test_series.py
@@ -0,0 +1,42 @@
+from math_series.series import fibonacci,lucas,sum_series
+# fibonacci tests
+def test_fibonacci_at_0():
+    assert fibonacci(0) == 0
+
+def test_fibonacci_at_2():
+    assert fibonacci(2) == 1
+
+def test_fibonacci_at_5():
+    assert fibonacci(5) == 5
+
+def test_fibonacci_at_7():
+    assert fibonacci(7) == 13
+
+def test_fibonacci_at_20():
+    assert fibonacci(20) == 6765
+
+# lucas tests
+def test_lucas_at_0():
+    assert lucas(0) == 2
+
+def test_lucas_at_2():
+    assert lucas(2) == 3
+
+def test_lucas_at_5():
+    assert lucas(5) == 11
+
+def test_lucas_at_7():
+    assert lucas(7) == 29
+
+def test_lucas_at_20():
+    assert lucas(20) == 15127
+
+# sum_series tests
+def test_sum_series_at_20_as_fibo():
+    assert sum_series(20) == 6765
+
+def test_sum_series_at_20_as_lucas():
+    assert sum_series(20, first=2, second=1) == 15127
+
+def test_sum_series_at_20_for_other_series():
+    assert sum_series(20, first=5, second=3) == 41200
